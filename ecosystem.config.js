e316694dcd741796fbb6c2e7ab97008fc8501efe
ryujiee <ryujiee@outlook.com>
2023-08-17 16:21:23 -0300

    test

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [{
+        name: 'Dashboard Telefonia',
+        script: 'server.js',
+        error_file : "err.log",
+        log_date_format: 'DDMMM HH:mm:ss',
+        watch: false,
+    }]
+};
